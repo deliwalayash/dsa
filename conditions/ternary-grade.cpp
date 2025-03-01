1830b8e4e0be70d1471239a348facfa8cc157f65
deliwalayash <deliwalayash2@gmail.com>
2025-03-01 14:41:56 +0530

    some chages made in ternary grade cpp

--- conditions/ternary-grade.cpp
+++ conditions/ternary-grade.cpp
@@ -5,11 +5,34 @@ using namespace std;
 int main(){
 
     int mark;
+    char grade;
 
     cout << "Enter Your Mark" << endl;
-
     cin >> mark;
 
-    (mark > 90) ? cout <<"A" : (mark >80) ? cout << "B" : (mark > 70) ? cout << "C" : cout <<"D";
+    grade = (mark > 90) ? 'A' : (mark >80) ? 'B' : (mark > 70) ?  'C' : 'D';
 
+    cout << grade << endl;
+
+    switch (grade)
+    { 
+        case 'A':
+            cout << "Excellent Work." << endl;
+            break;
+        case 'B':
+            cout << "Well Done." << endl;
+            break;
+        case 'C':
+            cout << "Good Job." << endl;
+            break;
+        case 'D':
+            cout << "You are Passed." << endl;
+            break;
+        case 'E':
+            cout << "You are Passed,But you can do Better." << endl;
+            break;
+        case 'F':
+            cout << "You are failed." << endl;
+            break;
+    }
 }
