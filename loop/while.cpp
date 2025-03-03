59ec78f15ca0f56ffd63c04af347ac483b0918b5
deliwalayash <deliwalayash2@gmail.com>
2025-03-03 19:08:36 +0530

    while loop

---
+++ loop/while.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i=1;
+
+    while(i <=5){
+        cout << i << " ).hello world" << endl;
+        i++;
+        i++;
+    }       
+}
